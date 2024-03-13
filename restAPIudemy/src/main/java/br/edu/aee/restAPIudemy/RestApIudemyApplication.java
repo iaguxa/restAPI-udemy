71ec3cb654582090a59bebf0478c39274bb6945e
Iago Santos <iago.santos@aluno.unievangelica.edu.br>
2024-03-13 15:35:32 -0300

    Firs commit in restAPI

---
+++ restAPIudemy/src/main/java/br/edu/aee/restAPIudemy/RestApIudemyApplication.java
@@ -0,0 +1,13 @@
+package br.edu.aee.restAPIudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApIudemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApIudemyApplication.class, args);
+	}
+
+}
